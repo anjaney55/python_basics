f7892fd8f60dde9826d32e0a5a0442001913f6ef
Anjaney <anjaneynaik5@gmail.com>
2025-09-16 13:28:34 +0530

    initial commit

---
+++ greater.py
@@ -0,0 +1,12 @@
+num1 =int(input("enter 1st number : "))
+
+num2 =int(input("enter 2nd number : "))
+
+# if(num1>=num2):
+
+#      print("True")
+
+# else:
+#     print("false")
+
+print(num1>=num2)
