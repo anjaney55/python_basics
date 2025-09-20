4ba0c2a1ded0ccb57d678c1455ca533712e3cdb8
Anjaney <anjaneynaik5@gmail.com>
2025-09-20 10:06:36 +0530

    initial commit

---
+++ set.py
@@ -0,0 +1,10 @@
+collection = {1,2,3,4,"Hi","Hello"}
+
+collection2 = {1,22,22,22,"Hi","Hello","Hi",4,5,5,5,5,4}
+
+print(collection)
+print(type(collection))
+print(len(collection))
+
+print(collection2)
+print(len(collection2))
