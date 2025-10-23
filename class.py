c900e7ba8599c706f161b8fb5260b56f2044f4f8
Anjaney <anjaneynaik5@gmail.com>
2025-10-23 11:32:44 +0530

    initial commit

---
+++ class.py
@@ -0,0 +1,13 @@
+#class and object creating
+
+class Student: 
+    name = "Anjaney"
+    age = 20
+
+print(Student.name,Student.age)
+
+#create an Object using class
+
+
+s1 = Student() #creat new s1 object using Student Class
+print(s1.name)
