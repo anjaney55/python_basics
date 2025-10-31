a2cec6c85a92803049374c57cec307fdc5303123
Anjaney <anjaneynaik5@gmail.com>
2025-10-31 11:45:33 +0530

    initial commit

--- practice.py
+++ practice.py
@@ -1,35 +1,40 @@
 
-'''create a clss of student which takes name & marks of 3 subject as argument in constructore then create a method to print average of marks'''
+# #to repplace Java into Phythod
 
-class Student:
+# with open("practice.txt","r+") as f :
+#     data = f.read()
+#     print(data)
 
-    def __init__(self,name,m1,m2,m3):
-        self.name = name
-        self.m1 = m1 
-        self.m2 = m2 
-        self.m3 = m3
+# new_data = data.replace("Java","Python")
 
-    def avg (self,m1,m2,m3):
-        sum = m1+m2+m3
-        avg = sum/3
-        return avg 
+# print(new_data)
 
-s1 = Student("kiran",76,87,54)
-print(s1.name)
-print("average marks is ",s1.avg(76,86,54)) 
-        
+# with open("practice.txt","w") as f:
+#     f.write(new_data)
 
-#Another method
-class Man:
-    def __init__(self,name,marks):
-        self.name = name
-        self.marks = marks
 
-    def get_Avg(self):
-        sum = 0
-        for i in self.marks:
-            sum += i
-        print("Hi",self.name,"Your avarage marks is ",sum/3)
-    
-m1 = Man("Vinod",[98,52,99])
-m1.get_Avg()
+# #to find the word
+
+# word = "learning"
+
+# with open("practice.txt","r") as f:
+#     new = f.read()
+#     if(new.find(word) != -1):
+#         print("Found")
+#     else:
+#         print("not found")
+
+
+#using function 
+
+def chec_for_word():
+    word = "xlearn"
+
+    with open("practice.txt","r") as f:
+        new = f.read()
+        if(new.find(word) != -1):
+            print("Found")
+        else:
+            print("not found")
+
+chec_for_word()     
